bc8e53f5e374ea17e4338b8a39bc4ff8339453eb
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2013-10-16 23:58:58 +0800

    First cut of the new converter and general infrastructure.

---
+++ TestsBase.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Autofac.Extras.Moq;
+using NUnit.Framework;
+
+namespace Hanlin.Tests
+{
+    public class TestsBase
+    {
+        public string AssetsFolder { get; set; }
+
+        protected string BasePath { get; set; }
+        protected AutoMock AutoMock { get; set; }
+
+        public TestsBase()
+        {
+            AssetsFolder = "TestCases";
+        }
+
+        protected string PathTo(string filename)
+        {
+            return Path.Combine(BasePath, filename); 
+        }
+
+        [SetUp]
+        protected virtual void Setup()
+        {
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.config"));
+
+            AutoMock = AutoMock.GetLoose();
+
+            BasePath = Path.Combine(Directory.GetCurrentDirectory(), AssetsFolder);
+        }
+    }
+}
