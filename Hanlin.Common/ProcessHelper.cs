85d11e1ec51989553e150b9a1791b01d059f6605
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2013-11-26 16:03:59 +0800

    Change Word exit strategy to kill-and-forget. Kill call no longer throws exception; it will be ignored. We will not require Word to exit gracefully for now.

--- Hanlin.Common/ProcessHelper.cs
+++ Hanlin.Common/ProcessHelper.cs
@@ -1,3 +1,5 @@
+using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
@@ -27,9 +29,24 @@ namespace Hanlin.Common
             return tries > 0;
         }
 
-        public static void KillWord()
+        public static bool TryKillWord()
         {
-            Kill("WINWORD");
+            const string wordProcName = "WINWORD";
+            try
+            {
+                Kill(wordProcName);
+            }
+            catch (Win32Exception)
+            {
+                // Win32Exception with an "Access is denied" message could occur when the process is already exiting.
+                // We ignore this exception but return a boolean value to indicate if any Word processes exist.
+                // If there are remaining processes, it means the kill operation failed, and the caller should act accordingly.
+                // If there is none, it means the kill operation likely succeeded.
+                // Tt could also mean that a Win32Exception had occurred, and that the process is already in exit state before the kill operation started.
+                // In this case, Word process should exit anyway even though an exception is thrown.
+            }
+
+            return Process.GetProcessesByName(wordProcName).Any();
         }
 
         public static void Kill(string procName)
