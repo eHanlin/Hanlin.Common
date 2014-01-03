fb9767c9188ed0c04515280ddc5424598489ae33
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2014-01-03 09:39:23 +0800

    Added exception classes.

---
+++ Hanlin.Common/EntityNotFoundException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hanlin.Common.Extensions;
+
+namespace Hanlin.Common
+{
+    public class EntityNotFoundException : HanlinApplicationException
+    {
+        public EntityNotFoundException(string message) : base(message)
+        {
+        }
+
+        public EntityNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+    public class EntityNotFoundException<T> : EntityNotFoundException
+    {
+        private const string MsgTemplate = "Cannot find entity of type {0} by lookup data: '{1}'.";
+
+        public EntityNotFoundException(object id) : base(string.Format(MsgTemplate, typeof(T).Name, id)) { }
+        public EntityNotFoundException(IEnumerable<object> ids) : base(string.Format(MsgTemplate, typeof(T).Name, ids.AsString())) { }
+        public EntityNotFoundException(object id, Exception e) : base(string.Format(MsgTemplate, typeof(T).Name, id), e) { }
+        public EntityNotFoundException(string message) : base(message) { }
+        public EntityNotFoundException(string message, Exception e) : base(message, e) { }
+    }
+}
---
+++ Hanlin.Common/HanlinApplicationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hanlin.Common
+{
+    public class HanlinApplicationException : Exception
+    {
+        public HanlinApplicationException(string message, params object[] objs) : base(string.Format(message, objs))
+        {
+            
+        }
+
+        public HanlinApplicationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
