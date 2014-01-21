36e10b0f720079c146985590bc9130628072d32b
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2014-01-21 17:04:14 +0800

    Added expanded version.

--- Hanlin.Common/Specifications/ISpecification.cs
+++ Hanlin.Common/Specifications/ISpecification.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Linq.Expressions;
+using LinqKit;
 
 namespace Hanlin.Common.Specifications
 {
     public interface ISpecification<T>
     {
         Expression<Func<T, bool>> GetPredicate();
+    }
+
+    public abstract class Specification<T> : ISpecification<T>
+    {
+        public Expression<Func<T, bool>> GetPredicateExpanded()
+        {
+            return GetPredicate().Expand();
+        } 
+
+        public abstract Expression<Func<T, bool>> GetPredicate();
     }
 }
