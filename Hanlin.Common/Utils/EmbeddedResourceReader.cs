f2afd57b83fafeb333afa7cb0db041f3d0ee8b45
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2014-05-26 12:44:18 +0800

    Added embedded resource util.

---
+++ Hanlin.Common/Utils/EmbeddedResourceReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanlin.Common.Utils
+{
+    public static class EmbeddedResourceReader
+    {
+        public static string GetEmbeddedResource(string name, string inNamespace, Assembly inAssembly)
+        {
+            string resource = null;
+            var resName = inNamespace + "." + name;
+
+            // See: http://stackoverflow.com/a/3314213/494297
+            using (var stream = inAssembly.GetManifestResourceStream(resName))
+            {
+                if (stream == null)
+                {
+                    throw new ArgumentException("Cannot open embedded resource with name: " + resName);
+                }
+
+                using (var reader = new StreamReader(stream))
+                {
+                    resource = reader.ReadToEnd();
+                }
+            }
+
+            return resource;
+        }
+    }
+}
--- Hanlin.Common/Utils/ImageUtils.cs
+++ Hanlin.Common/Utils/ImageUtils.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Text;
 using Hanlin.Common.Extensions;
 
-namespace Hanlin.Common
+namespace Hanlin.Common.Utils
 {
     public class ImageUtils
     {
