2b3359b8ebd8840565b3b83fb4e95262fb8013c2
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2014-07-08 09:09:12 +0800

    Add Lookups properties for retrieving data involved.

--- Hanlin.Common/EntityNotFoundException.cs
+++ Hanlin.Common/EntityNotFoundException.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using Hanlin.Common.Extensions;
@@ -19,11 +20,29 @@ namespace Hanlin.Common
 
     public class EntityNotFoundException<T> : EntityNotFoundException
     {
+        private readonly List<string> _lookups = new List<string>();
+
+        public IEnumerable<string> Lookups { get { return _lookups; } }
+
         private const string MsgTemplate = "Cannot find entity of type {0} by lookup data: '{1}'.";
 
-        public EntityNotFoundException(object id) : base(string.Format(MsgTemplate, typeof(T).Name, id)) { }
-        public EntityNotFoundException(IEnumerable<object> ids) : base(string.Format(MsgTemplate, typeof(T).Name, ids.AsString())) { }
-        public EntityNotFoundException(object id, Exception e) : base(string.Format(MsgTemplate, typeof(T).Name, id), e) { }
+        public EntityNotFoundException(object id) : base(string.Format(MsgTemplate, typeof (T).Name, id))
+        {
+            _lookups.Add(id.ToString());
+        }
+
+        public EntityNotFoundException(IReadOnlyCollection<string> lookups)
+            : base(string.Format(MsgTemplate, typeof (T).Name, string.Join(", ", lookups)))
+        {
+            _lookups.AddRange(lookups);
+        }
+
+        public EntityNotFoundException(object id, Exception e)
+            : base(string.Format(MsgTemplate, typeof (T).Name, id), e)
+        {
+            _lookups.Add(id.ToString());
+        }
+
         public EntityNotFoundException(string message) : base(message) { }
         public EntityNotFoundException(string message, Exception e) : base(message, e) { }
     }
