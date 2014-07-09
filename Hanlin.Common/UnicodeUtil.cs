200c5b27c0e77e9a87915fc657ea0df8a0e7add6
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2014-07-09 18:20:43 +0800

    Redone field code to text conversion.

---
+++ Hanlin.Common/UnicodeUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanlin.Common
+{
+    public static class UnicodeUtil
+    {
+        public const char FullWidthA = 'Ａ'; // /uFF21
+        public const char FullWidthZ = 'Ｚ';
+        public const char FullWidtha = 'ａ'; // /uFF41
+        public const char FullWidthz = 'ｚ';
+        public const char FullWidth0 = '０'; // /uFF10
+        public const char FullWidth9 = '９'; // /uFF19
+
+        public static char Normalize(char unicode)
+        {
+            if (unicode >= FullWidth0 && unicode <= FullWidth9)
+            {
+                return (char)(unicode - FullWidth0 + '0');
+            }
+
+            if (unicode >= FullWidthA && unicode <= FullWidthZ)
+            {
+                return (char)(unicode - FullWidthA + 'A');
+            }
+
+            if (unicode >= FullWidtha && unicode <= FullWidthz)
+            {
+                return (char)(unicode - FullWidtha + 'a');
+            }
+
+            return unicode;
+        }
+    }
+}
