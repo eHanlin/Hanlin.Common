8c82c440344fb12be62973e11138bf70f0293a93
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2014-07-11 15:42:32 +0800

    Combined two classes to create StringReplaceFilter based on Regex.

--- Hanlin.Common/Text/StringReplaceFilter.cs
+++ Hanlin.Common/Text/StringReplaceFilter.cs
@@ -1,23 +1,39 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
 
 namespace Hanlin.Common.Text
 {
     public class StringReplaceFilter : IStringFilter
     {
-        protected Dictionary<string, string> Patterns;
-
-        public StringReplaceFilter()
-        {
-            Patterns = new Dictionary<string, string>();
-        }
+        private readonly IDictionary<string, string> Patterns = new Dictionary<string, string>();
 
         public string Filter(string input)
         {
-            foreach (var pair in Patterns)
+            foreach (var pattern in Patterns)
             {
-                input = input.Replace(pair.Key, pair.Value);
+                input = Regex.Replace(input, pattern.Key, pattern.Value, RegexOptions.IgnoreCase);
             }
+
             return input;
+        }
+
+        public void Replace(string regex, string replaceRegex)
+        {
+            Patterns[regex] = replaceRegex;
+        }
+
+        public void Remove(string regex)
+        {
+            Patterns[regex] = string.Empty;
+        }
+    }
+
+    public class EmptyLineFilter : StringReplaceFilter
+    {
+        public EmptyLineFilter()
+        {
+            Remove("\r\n\r\n");
         }
     }
 }
