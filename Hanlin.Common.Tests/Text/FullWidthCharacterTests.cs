9964df12303e452aa9f6a59cba6a32fddb7bf971
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2014-08-05 10:05:58 +0800

    Added FullWidthCharacter utility class.

---
+++ Hanlin.Common.Tests/Text/FullWidthCharacterTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hanlin.Common.Text;
+using NUnit.Framework;
+
+namespace Hanlin.Common.Tests.Text
+{
+    class FullWidthCharacterTests
+    {
+        [TestCase(null, null)]
+        [TestCase("", "")]
+        [TestCase("a", "a")]
+        [TestCase("０", "0")]
+        [TestCase("９", "9")]
+        [TestCase("Ａ", "A")]
+        [TestCase("Ｚ", "Z")]
+        [TestCase("ａ", "a")]
+        [TestCase("ｚ", "z")]
+        public void ToHalfWidth(string input, string expected)
+        {
+            Assert.AreEqual(expected, FullWidthCharacter.ToHalfWidth(input));
+        }
+    }
+}
---
+++ Hanlin.Common/Text/FullWidthCharacter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanlin.Common.Text
+{
+    public static class FullWidthCharacter
+    {
+        public const char Half0 = '0';
+        public const char Half9 = '9';
+        public const char HalfA = 'A';
+        public const char HalfZ = 'Z';
+        public const char Halfa = 'a';
+        public const char Halfz = 'z';
+
+        public const char Full0 = '０'; // /uFF10
+        public const char Full9 = '９'; // /uFF19
+        public const char FullA = 'Ａ'; // /uFF21
+        public const char FullZ = 'Ｚ';
+        public const char Fulla = 'ａ'; // /uFF41
+        public const char Fullz = 'ｚ';
+
+        public static string ToHalfWidth(string text)
+        {
+            if (text == null) return null;
+
+            return string.Join(string.Empty, text.Select(ToHalfWidth));
+        }
+
+        public static char ToHalfWidth(char wchar)
+        {
+            if (wchar >= Full0 && wchar <= Full9)
+            {
+                return (char)(wchar - Full0 + '0');
+            }
+
+            if (wchar >= FullA && wchar <= FullZ)
+            {
+                return (char)(wchar - FullA + 'A');
+            }
+
+            if (wchar >= FullA && wchar <= FullZ)
+            {
+                return (char)(wchar - FullA + 'A');
+            }
+
+            if (wchar >= Fulla && wchar <= Fullz)
+            {
+                return (char)(wchar - Fulla + 'a');
+            }
+
+            return wchar;
+        }
+    }
+}
