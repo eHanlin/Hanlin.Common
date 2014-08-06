83f441bd0da3b560bb9ee0065e028bd1b01c2d91
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2014-08-07 00:11:12 +0800

    Added Size.Scale extension.

---
+++ Hanlin.Common/Extensions/SizeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanlin.Common.Extensions
+{
+    public static class SizeExtensions
+    {
+        public static Size Scale(this Size size, float scale)
+        {
+            if (scale <= 0) return new Size(size.Width, size.Height);
+
+            var aspect = (double)size.Width / size.Height;
+
+            var width = Math.Floor(size.Width * scale);
+            var height = width / aspect;
+
+            var newSize = new Size((int)width, (int)height);
+
+            return newSize;
+        }
+    }
+}
