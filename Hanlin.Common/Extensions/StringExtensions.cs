ec8b3988c8dc2dc262594130a55aa9ea15280559
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2014-08-22 10:27:38 +0800

    Added ToLowerOrDefault.

---
+++ Hanlin.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanlin.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToLowerOrDefault(this string str)
+        {
+            if (str == null) return null;
+
+            return str.ToLowerInvariant();
+        }
+    }
+}
