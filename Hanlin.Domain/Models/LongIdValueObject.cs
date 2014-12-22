4f23eca1a387a83050da1d6135e5a674acbe801e
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2014-12-22 09:15:29 +0800

    Moved LongIdValueObject here from ItemBank project.

---
+++ Hanlin.Domain/Models/LongIdValueObject.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Hanlin.Domain.Models
+{
+    public class LongIdValueObject : GenericValueObject<long>
+    {
+        public LongIdValueObject(long id)
+            : base(id)
+        {
+        }
+
+        public LongIdValueObject(string idStr)
+            : base(long.Parse(idStr))
+        {
+        }
+    }
+}
