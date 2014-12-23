add0cdbe82deb51ae1d9344500eacab1e8265590
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2014-12-23 16:27:15 +0800

    Moved DatabaseConfigurationSection here from ItemBank project.

---
+++ Hanlin.Common/Configuration/DatabaseConfigurationSection.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+
+namespace Hanlin.Common.Configuration
+{
+    public class DatabaseConfigurationSection : ConfigurationSection
+    {
+        private const string UriAttribute = "uri";
+        private const string DatabaseAttribute = "database";
+
+        [ConfigurationProperty(UriAttribute, IsRequired = true)]
+        public string Uri
+        {
+            get { return (string)this[UriAttribute]; }
+            set { this[UriAttribute] = value; }
+        }
+
+        [ConfigurationProperty(DatabaseAttribute)]
+        public string Database
+        {
+            get { return (string)this[DatabaseAttribute]; }
+            set { this[DatabaseAttribute] = value; }
+        }
+    }
+}
