e77194012fc62c0c257075578a337e393d4da0f3
sparrow.jang <sparrow.jang@gmail.com>
2016-09-11 22:12:03 +0800

    Added a reflection util

---
+++ Hanlin.Common.Tests/Utils/ReflectionUtilsTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+using Hanlin.Common.Utils;
+using NUnit.Framework;
+
+namespace Hanlin.Common.Tests.Utils
+{
+    internal class Company
+    {
+        public string Name { set; get; }
+        public string ValidationUnit;
+    }
+
+
+    public class ReflectionUtilsTests
+    {
+        [TestCase("Hanlin", "name", true, false)]
+        [TestCase("Hanlin", "Name", false, false)]
+        public void Property(string name, string fieldName, bool isUpperCaes, bool isLowerCase)
+        {
+            var company = new Company {Name = name};
+            Assert.AreEqual(name, ReflectionUtils.GetPropertyValue<string>(company, fieldName, isUpperCaes, isLowerCase));
+        }
+
+        [TestCase]
+        public void PropertyNames()
+        {
+            var company = new Company();
+            var propNames = ReflectionUtils.GetPropertyNames(company);
+            Assert.AreEqual(true, propNames.Contains("Name"));
+            Assert.AreEqual(1, propNames.Count());
+        }
+
+        [TestCase("gov", "validationUnit", true, false)]
+        [TestCase("gov", "ValidationUnit", false, false)]
+        public void Field(string validationUnit, string fieldName, bool isUpperCaes, bool isLowerCase)
+        {
+            var company = new Company();
+            company.ValidationUnit = validationUnit;
+            Assert.AreEqual(validationUnit, ReflectionUtils.GetFieldValue<string>(company, fieldName, isUpperCaes, isLowerCase));
+        }
+
+        [TestCase]
+        public void FieldNames()
+        {
+            var company = new Company();
+            var fieldNames = ReflectionUtils.GetFieldNames(company);
+            Assert.AreEqual(true, fieldNames.Contains("ValidationUnit"));
+            Assert.AreEqual(1, fieldNames.Count());
+        }
+    }
+}
---
+++ Hanlin.Common/Utils/ReflectionUtils.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Hanlin.Common.Utils
+{
+    public class ReflectionUtils
+    {
+        private static string ToFirstLowerOrUpperCase(string name, bool isLower)
+        {
+            if (name.Length > 0)
+            {
+                var firstChar = (isLower ? char.ToLower(name[0]) : char.ToUpper(name[0]));
+
+                return name.Length > 1 ? firstChar + name.Substring(1) : firstChar.ToString();
+            }
+            return name;
+        }
+
+        private static string GetFieldName(string field, bool? isUpperCase, bool? isLowerCase)
+        {
+            return isUpperCase.HasValue && (bool)isUpperCase ? ToFirstLowerOrUpperCase(field, false) :
+                isLowerCase.HasValue && (bool)isLowerCase ? ToFirstLowerOrUpperCase(field, true) : field;
+        }
+
+        public static T GetPropertyValue<T>(object instance, string field, bool isUpperCase = false, bool isLowerCase = false)
+        {
+            var fieldName = GetFieldName(field, isUpperCase, isLowerCase);
+            var instanceType = instance.GetType();
+            var propInfo = instanceType.GetProperty(fieldName);
+            return (T)propInfo.GetValue(instance);
+        }
+
+        public static T GetFieldValue<T>(object instance, string field, bool isUpperCase = false, bool isLowerCase = false)
+        {
+            var fieldName = GetFieldName(field, isUpperCase, isLowerCase);
+            var instanceType = instance.GetType();
+            var fieldInfo = instanceType.GetField(fieldName);
+            return (T)fieldInfo.GetValue(instance);
+        }
+
+        public static IEnumerable<string> GetPropertyNames(object instance, bool isUpperCase = false, bool isLowerCase = false)
+        {
+            return instance.GetType().GetProperties().Select(s => GetFieldName(s.Name, isUpperCase, isLowerCase));
+        }
+
+        public static IEnumerable<string> GetFieldNames(object instance, bool isUpperCase = false, bool isLowerCase = false)
+        {
+            return instance.GetType().GetFields().Select(s => GetFieldName(s.Name, isUpperCase, isLowerCase));
+        }
+    }
+}
