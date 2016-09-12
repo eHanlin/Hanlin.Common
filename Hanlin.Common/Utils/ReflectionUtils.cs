072d56bd0ba98bb9b5f43bbcbe79b18e4936b6fd
sparrow.jang <sparrow.jang@gmail.com>
2016-09-12 10:31:23 +0800

    Rename

--- Hanlin.Common/Utils/ReflectionUtils.cs
+++ Hanlin.Common/Utils/ReflectionUtils.cs
@@ -18,36 +18,36 @@ namespace Hanlin.Common.Utils
             return name;
         }
 
-        private static string GetFieldName(string field, bool? isUpperCase, bool? isLowerCase)
+        private static string GetFieldName(string field, bool? isFirstUpperCase, bool? isFirstLowerCase)
         {
-            return isUpperCase.HasValue && (bool)isUpperCase ? ToFirstLowerOrUpperCase(field, false) :
-                isLowerCase.HasValue && (bool)isLowerCase ? ToFirstLowerOrUpperCase(field, true) : field;
+            return isFirstUpperCase.HasValue && (bool)isFirstUpperCase ? ToFirstLowerOrUpperCase(field, false) :
+                isFirstLowerCase.HasValue && (bool)isFirstLowerCase ? ToFirstLowerOrUpperCase(field, true) : field;
         }
 
-        public static T GetPropertyValue<T>(object instance, string field, bool isUpperCase = false, bool isLowerCase = false)
+        public static T GetPropertyValue<T>(object instance, string field, bool isFirstUpperCase = false, bool isFirstLowerCase = false)
         {
-            var fieldName = GetFieldName(field, isUpperCase, isLowerCase);
+            var fieldName = GetFieldName(field, isFirstUpperCase, isFirstLowerCase);
             var instanceType = instance.GetType();
             var propInfo = instanceType.GetProperty(fieldName);
             return (T)propInfo.GetValue(instance);
         }
 
-        public static T GetFieldValue<T>(object instance, string field, bool isUpperCase = false, bool isLowerCase = false)
+        public static T GetFieldValue<T>(object instance, string field, bool isFirstUpperCase = false, bool isFirstLowerCase = false)
         {
-            var fieldName = GetFieldName(field, isUpperCase, isLowerCase);
+            var fieldName = GetFieldName(field, isFirstUpperCase, isFirstLowerCase);
             var instanceType = instance.GetType();
             var fieldInfo = instanceType.GetField(fieldName);
             return (T)fieldInfo.GetValue(instance);
         }
 
-        public static IEnumerable<string> GetPropertyNames(object instance, bool isUpperCase = false, bool isLowerCase = false)
+        public static IEnumerable<string> GetPropertyNames(object instance, bool isFirstUpperCase = false, bool isFirstLowerCase = false)
         {
-            return instance.GetType().GetProperties().Select(s => GetFieldName(s.Name, isUpperCase, isLowerCase));
+            return instance.GetType().GetProperties().Select(s => GetFieldName(s.Name, isFirstUpperCase, isFirstLowerCase));
         }
 
-        public static IEnumerable<string> GetFieldNames(object instance, bool isUpperCase = false, bool isLowerCase = false)
+        public static IEnumerable<string> GetFieldNames(object instance, bool isFirstUpperCase = false, bool isFirstLowerCase = false)
         {
-            return instance.GetType().GetFields().Select(s => GetFieldName(s.Name, isUpperCase, isLowerCase));
+            return instance.GetType().GetFields().Select(s => GetFieldName(s.Name, isFirstUpperCase, isFirstLowerCase));
         }
     }
 }
