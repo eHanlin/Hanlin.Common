1ecd899d34a52e24e3b800f777f0720c071e11da
sparrow.jang <sparrow.jang@gmail.com>
2016-09-14 19:06:41 +0800

    Added a IQueryable extension

---
+++ Hanlin.Common/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Hanlin.Common.Extensions
+{
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<T> OrderByField<T>(this IQueryable<T> q, string sortField, bool ascending)
+        {
+            var param = Expression.Parameter(typeof(T), "p");
+            var prop = Expression.Property(param, sortField);
+            var exp = Expression.Lambda(prop, param);
+            string method = ascending ? "OrderBy" : "OrderByDescending";
+            Type[] types = new Type[] { q.ElementType, exp.Body.Type };
+            var mce = Expression.Call(typeof(Queryable), method, types, q.Expression, exp);
+            return q.Provider.CreateQuery<T>(mce);
+        }
+    }
+}
