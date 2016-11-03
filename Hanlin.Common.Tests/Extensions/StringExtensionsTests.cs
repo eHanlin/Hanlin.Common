9e9deb2c3f6afec004e9eee2792e4c7da459ba42
sparrow.jang <sparrow.jang@gmail.com>
2016-11-03 12:39:50 +0800

    Added a test class for first lower and upper cases

---
+++ Hanlin.Common.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Hanlin.Common.Extensions;
+using NUnit.Framework;
+
+namespace Hanlin.Common.Tests.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [TestCase(null, null)]
+        [TestCase("Test", "test")]
+        [TestCase("TestCase", "testCase")]
+        public void ToFirstLower(string input, string expected)
+        {
+            Assert.AreEqual(expected, input.ToFirstLowerOrDefault());
+        }
+
+        [TestCase(null, null)]
+        [TestCase("test", "Test")]
+        [TestCase("testCase", "TestCase")]
+        public void ToFirstUpper(string input, string expected)
+        {
+            Assert.AreEqual(expected, input.ToFirstUpperOrDefault());
+        }
+    }
+}
