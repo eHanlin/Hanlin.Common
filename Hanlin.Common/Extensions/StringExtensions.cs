a41db030e349ca91273f9428d5d207882e1a8f9d
sparrow.jang <sparrow.jang@gmail.com>
2016-11-03 12:38:58 +0800

    Added  2 methods for first lower and upper cases

--- Hanlin.Common/Extensions/StringExtensions.cs
+++ Hanlin.Common/Extensions/StringExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Hanlin.Common.Extensions
@@ -13,6 +14,28 @@ namespace Hanlin.Common.Extensions
             if (str == null) return null;
 
             return str.ToLowerInvariant();
+        }
+
+        private static string ToLowerOrUpper(this string str, bool isLower)
+        {
+            if (str == null) return null;
+            if (str.Length > 0)
+            {
+                var firstChar = (isLower ? char.ToLower(str[0]) : char.ToUpper(str[0]));
+
+                return str.Length > 1 ? firstChar + str.Substring(1) : firstChar.ToString();
+            }
+            return str;
+        }
+
+        public static string ToFirstLowerOrDefault(this string str)
+        {
+            return ToLowerOrUpper(str, true);
+        }
+
+        public static string ToFirstUpperOrDefault(this string str)
+        {
+           return ToLowerOrUpper(str, false);
         }
     }
 }
