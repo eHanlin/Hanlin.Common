9e3bcaee9982a0b7903bee3f6bba6fe47316e657
sparrow.jang <sparrow.jang@gmail.com>
2017-05-10 17:11:18 +0800

    Release 1.0.24 version

--- Hanlin.Common/Properties/AssemblyInfo.cs
+++ Hanlin.Common/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.23")]
+[assembly: AssemblyInformationalVersion("1.0.24")]
 
 [assembly: InternalsVisibleTo("Hanlin.Common.Tests")]
