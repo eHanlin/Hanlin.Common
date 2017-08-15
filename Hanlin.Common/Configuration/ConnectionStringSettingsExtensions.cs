03d6587276ffd0e7ab0d7f983af1f22945d42e18
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2017-08-15 10:09:05 +0800

    Release version 1.2.0: Added ConnectionStringSettingsExtensions

---
+++ Hanlin.Common/Configuration/ConnectionStringSettingsExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace Hanlin.Common.Configuration
+{
+    public static class ConnectionStringSettingsExtensions
+    {
+        private const char KeyValueSeparator = '=';
+        private const char EntrySeparator = ';';
+
+        public static IDictionary<string, string> GetConnectionProperties(this ConnectionStringSettings connSettings)
+        {
+            if (connSettings == null)
+            {
+                throw new ArgumentNullException(nameof(connSettings));
+            }
+
+            return ParseConnectionString(connSettings.ConnectionString);
+        }
+
+        private static IDictionary<string, string> ParseConnectionString(string connStr)
+        {
+            if (connStr.Contains(KeyValueSeparator) || connStr.Contains(EntrySeparator))
+            {
+                return connStr.Split(new[] { EntrySeparator }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(entry => entry.Split(new[] { KeyValueSeparator }, StringSplitOptions.RemoveEmptyEntries))
+                    .ToDictionary(pair => pair[0], pair => pair.Length > 1 ? pair[1] : null);
+            }
+            else
+            {
+                return new Dictionary<string, string>();
+            }
+        }
+    }
+}
