f386206d45fa423caf9994ad2b7c191b9240e16f
Jack Chen <kuanfuchen@ms1.hanlin.com.tw>
2017-09-21 10:50:41 +0800

    Added a version of BatchOperation without result type

--- Hanlin.Common/Tasks/BatchOperation.cs
+++ Hanlin.Common/Tasks/BatchOperation.cs
@@ -4,6 +4,11 @@ using System.Linq;
 
 namespace Hanlin.Common.Tasks
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T">The type of the elements of the input collection.</typeparam>
+    /// <typeparam name="TR">The operation result type.</typeparam>
     public class BatchOperation<T, TR>
     {
         public int BatchSize { get; set; }
@@ -22,6 +27,32 @@ namespace Hanlin.Common.Tasks
                 var batchInput = input.Skip(i * BatchSize).Take(BatchSize).ToArray();
 
                 operation(batchInput, operationResults);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T">The type of the elements of the input collection.</typeparam>
+    public class BatchOperation<T>
+    {
+        public int BatchSize { get; set; }
+
+        public BatchOperation(int batchSize = 1000)
+        {
+            BatchSize = batchSize;
+        }
+
+        public void Execute(IReadOnlyCollection<T> input, Action<IReadOnlyCollection<T>> operation)
+        {
+            var numberOfBatches = (int)Math.Ceiling(input.Count / (double)BatchSize);
+
+            for (int i = 0; i < numberOfBatches; i++)
+            {
+                var batchInput = input.Skip(i * BatchSize).Take(BatchSize).ToArray();
+
+                operation(batchInput);
             }
         }
     }
