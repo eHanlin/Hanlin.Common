f4c4fb10516da1d164482a1e7bf12c5332306d62
sparrow.jang <sparrow.jang@gmail.com>
2019-03-28 18:28:35 +0800

    Added CmdHelper

---
+++ Hanlin.Common/CmdHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Hanlin.Common
+{
+    public class CmdHelper
+    {
+        public static Process Shutdown(bool force = true, int second = 10)
+        {
+            var opts = new List<string> { "-s" };
+
+            if (force) opts.Add("-f");
+
+            opts.Add("-t");
+            opts.Add("10");
+
+            return Process.Start("shutdown", string.Join(" ", opts));
+        }
+    }
+}
