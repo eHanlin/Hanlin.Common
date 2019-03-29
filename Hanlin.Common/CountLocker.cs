f903d4f5b72167eefde527fd45d8e92ed9b2f786
sparrow.jang <sparrow.jang@gmail.com>
2019-03-29 15:19:31 +0800

    Added a CountLocker class

---
+++ Hanlin.Common/CountLocker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace Hanlin.Common
+{
+    public class CountLocker
+    {
+        public object Locker { get; } = new Object();
+        private int Timout { get; set; }
+        private readonly Func<bool> _stop;
+        private readonly Func<object> _done;
+        private int _count = 0;
+
+        public CountLocker(Func<bool> stop, Func<object> done, int timeout)
+        {
+            Timout = timeout;
+            _stop = stop;
+            _done = done;
+            _count = 0;
+            new Thread(Release).Start();
+        }
+
+        public void Inc()
+        {
+            _count++;
+        }
+
+        public void Desc()
+        {
+            _count--;
+        }
+
+        private void Release()
+        {
+            var stop = true;
+
+            do
+            {
+                Thread.Sleep(Timout * 2);
+
+                lock (Locker)
+                {
+                    Thread.Sleep(Timout / 120);
+
+                    if (_count > 0 || !_stop())
+                    {
+                        stop = false;
+                    }
+                    else
+                    {
+                        stop = true;
+                    }
+                }
+
+            } while (!stop);
+
+            _done();
+        }
+    }
+}
