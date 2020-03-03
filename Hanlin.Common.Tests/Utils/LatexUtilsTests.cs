a52f7d38e6dc68fca05aa5595ed96a5dfae42101
sparrow.jang <sparrow.jang@gmail.com>
2020-03-03 12:59:29 +0800

    Added a LatexUtils.

---
+++ Hanlin.Common.Tests/Utils/LatexUtilsTests.cs
@@ -0,0 +1,18 @@
+﻿using Hanlin.Common.Utils;
+using NUnit.Framework;
+
+namespace Hanlin.Common.Tests.Utils
+{
+    public class LatexUtilsTests
+    {
+        [TestCase(@"log_{x}", true)]
+        [TestCase(@"\overline{x}", true)]
+        [TestCase(@"\frac {x}{y}", true)]
+        [TestCase(@"X_{y}", true)]
+        [TestCase(@"X^{y}", true)]
+        public void Property(string text, bool result)
+        {
+            Assert.IsTrue(LatexUtils.IsValid(text) == result);
+        }
+    }
+}
---
+++ Hanlin.Common/Utils/LatexUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Hanlin.Common.Utils
+{
+    public class LatexUtils
+    {
+        public static bool IsValid(string text)
+        {
+            var r = new Regex(@"(\\[a-zA-Z]+)|(_\{)|(\^{)");
+            return r.Match(text).Success;
+        }
+    }
+}
